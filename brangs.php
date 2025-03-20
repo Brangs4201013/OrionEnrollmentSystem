38a39e1d854fd911c464d35099c93d57b159bc38
BRANGS4201013 <limotanbryll420@gmail.com>
2025-03-20 15:31:50 +0800

    first version

---
+++ brangs.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>THIS IS BRANGS</title>
+</head>
+<body>
+        <h1>THIS IS BRANGS</h1>
+        <h2>asdfghjk</h2>
+</body>
+</html>
